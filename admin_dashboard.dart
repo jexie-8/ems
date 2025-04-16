1faacf55e6cbe81d4af0da7546c731ed67bd97ce
miroshub <miroo4276@gmail.com>
2025-04-16 22:34:50 +0200

    This is the admin ashboard

---
+++ admin_dashboard.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'createevent.dart'; // If you're using CreateEventPage
+
+class AdminDashboardPage extends StatelessWidget {
+  const AdminDashboardPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Admin Dashboard'),
+        backgroundColor: Colors.purpleAccent,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const CreateEventPage(),
+                  ),
+                );
+              },
+              child: const Text('Create Event'),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                print("View Events button pressed");
+              },
+              child: const Text('View Events'),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                print("View Attendee button pressed");
+              },
+              child: const Text('View Attendee'),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                print("Manage Tickets button pressed");
+              },
+              child: const Text("Manage Tickets"),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                print("Send notifications button pressed");
+              },
+              child: const Text("Send notifications"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
